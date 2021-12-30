8bb8a939084dcef4287d7366b9387d5e023fe5c6
김서영 <55133538+ksy990628@users.noreply.github.com>
2021-12-31 01:15:57 +0900

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask, render_template
+
+#Flask 객체 인스턴스 생성
+app = Flask(__name__)
+
+@app.route('/') # 접속하는 url
+def index():
+  return render_template('index.html')
+
+if __name__=="__main__":
+  app.run(debug=True)
+  # host 등을 직접 지정하고 싶다면
+  # app.run(host="127.0.0.1", port="5000", debug=True)
