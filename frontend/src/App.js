5f16f15cd850a78e94fa0be17356ad4470122caf
suaj <rmdwjdmind@gmail.com>
2022-01-04 07:17:41 +0900

    frontend fileUpload axios Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,45 @@
+import React, {useState} from 'react';
 import axios from 'axios';
 
 const App = () => {
-  const setBtn = () => {
-  axios.post("http://localhost:5000/api/checkCors").then(
+  
+   const [fileUrl, setFileUrl] = useState(null);
+  
+   function processImage(event){
+    event.preventDefault();
+
+    const imageFile = event.target.files[0];
+
+    const imageUrl = URL.createObjectURL(imageFile);
+    setFileUrl(imageUrl)
+
+    
+  }
+
+  const setBtn = (fileUrl) => {
+    const formData = new FormData()
+    const config = {
+      header: {'content-type': 'multipart/form-data'}
+  }
+  formData.append('file',fileUrl)
+
+      
+
+  axios.post("http://localhost:5000/api/checkCors" , formData, config).then(
     response=>{
-      console.log(response.data);
+      if (response.data.success) {
+        alert('파일 저장 성공?!')
+    } else {
+        alert('파일 저장 실패')
+    }
     }
   )}
+
   return (
-    <div className="App">
-      <p>Hey</p>
-      <button onClick={setBtn}>2A22</button>
+    <div className='App'>
+      <img alt="hey" src={fileUrl}></img>
+    <input id="profile-upload" type="file" accept="image/*" onChange={processImage}/>
+    <button onClick={setBtn}>2A22</button>
     </div>
   );
 }
