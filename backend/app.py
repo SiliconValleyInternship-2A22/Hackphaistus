1595a4608d334c7abeb3e82d15171176aec3f1c1
김서영 <ksy990628@gmail.com>
2022-01-07 10:18:13 +0900

    Connect tmp detectLandmarks.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,8 @@
 from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 from connection import s3_connection
-from config import BUCKET_NAME
+from config import BUCKET_NAME, LOCATION
+import detectLandmarks
 
 #Flask 객체 인스턴스 생성
 app = Flask(__name__)
@@ -22,28 +23,29 @@ def fileUpload():
   if request.method == 'POST':
     global file
     file = request.files['file']
-
+    filename = file.filename
     # AWS S3 bucket
     s3 = s3_connection()
     s3.put_object(Bucket = BUCKET_NAME,Body = file,Key = file.filename,
     	ContentType = file.content_type)
-
-    global path
-    path = "s3://siliconvalleyinternship2a22/"+file.filename
-    return jsonify({"success": True, "file": "Received", "name": file.filename, "path": path})  
+    s3path = "s3://{BUCKET_NAME}/"+filename
+    global s3url
+    s3url = f'https://{BUCKET_NAME}.s3.{LOCATION}.amazonaws.com/{filename}'
+    calculateRatio()
+    return jsonify({"success": True, "file": "Received", "name": filename, "path": s3path})  
     
 
 # 받은 img 파일 -> Flask -> RabbitMQ -> AI -> Flask
 def calculateRatio():
+  result = detectLandmarks.main()
+  print(result)
   return 0
 
 
 @app.route("/api/printResult", methods=["POST"])
 def printResult():
-  abilities = "xxx"
   if request.method == "POST":
-    abilities = "abc"
-  return abilities
+    return 'process'
 
 
 if __name__=="__main__":
