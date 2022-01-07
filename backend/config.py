0dca8385fce3734c3416e631afb46c027c8f7b68
김서영 <ksy990628@gmail.com>
2022-01-07 15:48:06 +0900

    Debug config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 # config.py
 ...
-AWS_ACCESS_KEY = "AKIAQQ3OLIGRPDEY3TMC"
-AWS_SECRET_KEY = "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"
-BUCKET_NAME = "siliconvalleyinternship2a22"
-LOCATION = "ap-northeast-2"
+AWS_ACCESS_KEY = ""
+AWS_SECRET_KEY = ""
+BUCKET_NAME = ""
+LOCATION = ""
 ...
