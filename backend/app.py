36c2af38977984b3775d64a33952e3581824554a
김서영 <ksy990628@gmail.com>
2022-01-12 03:23:21 +0900

    Create RabbitMQ-client

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,26 @@
 # app.py
 from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
-#from flask_sqlalchemy import SQLAlchemy
 import pymysql
-from flask_restx import Resource, Api, fields, reqparse  # 추가한 부분
+from flask_restx import Resource, Api # Swagger
 from werkzeug.datastructures import FileStorage
 from connection import s3_connection
 from config import BUCKET_NAME, LOCATION
-from via import calculateRatio
 
-#Flask 객체 인스턴스 생성
+import pika
+import sys
+sys.path.append('../ai')
+from via import checkRabbitMQ
+
+#Swagger
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='task_queue', durable=True)
+
+# Flask 객체 인스턴스 생성
 app = Flask(__name__)
 
-#추가 한 내용
+# Swagger API
 api = Api(app,version='1.0',title='Hackphaistus API',description='Hackphaistus REST API 문서')
 ns = api.namespace('api',description='Hackphaistus API')  
 parser = ns.parser()
@@ -21,18 +29,13 @@ result_parser = ns.parser()
 
 # CORS(app)
 CORS(app, resources={r'*':{'origins': 'http://localhost:3000'}})
-# database 설정파일
-# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:1234@localhost:3306/hackphaistus"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# db = SQLAlchemy(app)
+# MySQL
 db = pymysql.connect(host='localhost',
                      port=3306,
                      user='root',
                      passwd='1234',
                      db='hackphaistus',
                      charset='utf8')
-
-
 @ns.route('/')                 
 class Main(Resource):
   def post():
@@ -60,17 +63,27 @@ class fileUpload(Resource):
     # AWS S3 bucket
     s3 = s3_connection()
     s3.put_object(Bucket = BUCKET_NAME,Body = file,Key = file.filename,ContentType = file.content_type)
-    dataUrl = [BUCKET_NAME,filename,filename]
+    dataUrl = BUCKET_NAME+"-"+filename+"-"+filename
     #s3url = f'https://{BUCKET_NAME}.s3.{LOCATION}.amazonaws.com/{filename}'
     skills = sendToDetect(dataUrl)
-    #print(skills)
     return jsonify({"skills": skills})
     #return jsonify({"skills":[67, 70, 55, 52, 67, 68]})  
 
 # 받은 img 파일 -> Flask -> RabbitMQ (-> Python -> AI -> Python) -> Flask
 def sendToDetect(url):
-  skills = calculateRatio(url)
-  return skills
+  message = str(url)
+  channel.basic_publish(exchange='',routing_key='task_queue',body=message,
+    properties=pika.BasicProperties(
+        delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+    ))
+  print(" [x] Sent %r" % message)
+  checkRabbitMQ()
+  #return skills
+  #connection.close()
+
+def receiveFromDetect(r):
+  global result
+  result = r
 
 @ns.route('/printResult')
 class printResult(Resource):
@@ -80,8 +93,7 @@ class printResult(Resource):
   @ns.response(500, "서버에서 에러 발생")
 
   def post(self):     
-    return 'process'
-
+    return result
 
 if __name__=="__main__":
   # host 등을 직접 지정하고 싶다면
