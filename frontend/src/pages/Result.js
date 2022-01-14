34809edaf57d02537c7d215708aa16b791a120f1
suaj <rmdwjdmind@gmail.com>
2022-01-14 20:45:02 +0900

    web publishing main,upload page

---
+++ frontend/src/pages/Result.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../css/Result.css";
+import styled from 'styled-components';
+
+function Result(){
+
+    return(
+        <div className='defalutBGC3'>
+            
+        </div>
+    );
+}
+
+export default Result;
