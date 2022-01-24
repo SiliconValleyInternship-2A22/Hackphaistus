1f701be334c85f7e0488784f6b7c81f4c68d1732
suaj <rmdwjdmind@gmail.com>
2022-01-24 11:48:10 +0900

    chart update

--- frontend/src/components/chart.js
+++ frontend/src/components/chart.js
@@ -2,36 +2,37 @@ import React, { PureComponent, useState, useEffect } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 
-// const Arr = (props) => {
-//   const [result,setResult] = useState(props.skills);
-
+const AreachartArr = (props) => {
+  const [result,setResult] = useState(props.skills);
+  
   const data = [
     {
       name: '지혜',
-      score: 10,
+      score: result[0],
     },
     {
       name: '매력',
-      score: 20,
+      score: result[1],
     },
     {
       name: '통솔력',
-      score: 30,
+      score: result[2],
     },
     {
       name: '열정',
-      score: 20,
+      score: result[3],
     },
     {
       name: '사회성',
-      score: 40,
+      score: result[4],
     },
     {
       name: '신뢰도',
-      score: 50,
-      //score:result[5]
+      score: result[5],
     },
   ];
+}
+
 
 export default class Chart extends PureComponent {
   static demoUrl = 'https://codesandbox.io/s/simple-area-chart-4ujxw';
@@ -56,7 +57,7 @@ export default class Chart extends PureComponent {
           <XAxis dataKey="name" fontSize={11} />
           <YAxis fontSize={10}/>
           <Tooltip />
-          <Area type="monotone" dataKey={data} stroke="#da3939" fill="#661E1E" />
+          <Area type="monotone" dataKey="score" stroke="#da3939" fill="#661E1E" />
         </AreaChart>
       </ResponsiveContainer>
     );
